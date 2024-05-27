13d7a1648f3cb7cec2aa7651c26afe2294cf9e19
valen1965 <vladvazhnenko@gmail.com>
2024-05-27 03:23:51 +0300

    commit-23

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import psycopg2
 
 load_dotenv(f"{os.getcwd()}/{'.env'}")
 # WTF_SECRET_KEY = os.environ.get("WTF_SECRET_KEY")
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL=os.environ.get("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 WTF_SECRET_KEY="it is super secret key of mine"
